63c0fa0cb8864245f99bdb74f478d5af379f5d61
Gavin0717 <yf2699@columbia.edu>
2024-06-29 02:31:57 +0800

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -8,20 +8,19 @@ Created on Wed Jun 26 02:55:06 2024
 import numpy as np
 
 def FizzBuzz(start, finish):
-    
-    fbRange = np.arange(start, finish + 1)
-    
-    result = fbRange.astype(str)
-    
-    result = np.where((fbRange % 3 == 0) & (fbRange % 5 == 0), "fizzbuzz", result)
-   
-    result = np.where((fbRange % 3 == 0) & (fbRange % 5 != 0), "fizz", result)
+    fbRange = np.arange(start, finish + 1)  
+    result = np.empty(fbRange.shape, dtype=object)  
 
-    result = np.where((fbRange % 3 != 0) & (fbRange % 5 == 0), "buzz", result)
-    
+
+    result[(fbRange % 3 == 0) & (fbRange % 5 == 0)] = "FizzBuzz"
+    result[(fbRange % 3 == 0) & (fbRange % 5 != 0)] = "Fizz"
+    result[(fbRange % 3 != 0) & (fbRange % 5 == 0)] = "Buzz"
+
+
+    result[result == None] = fbRange[result == None]
 
     return result
 
 
-print(FizzBuzz(31, 45))
+FizzBuzz(31, 45)
 
